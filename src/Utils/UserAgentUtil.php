66c5d96c59bdf438000a450345ad05d6f9a88c8e
phuluong <phult.contact@gmail.com>
2024-10-26 23:07:27 +0700

    feature: add deprecated functions

--- src/Utils/UserAgentUtil.php
+++ src/Utils/UserAgentUtil.php
@@ -45,6 +45,30 @@ class UserAgentUtil
         // }
         return $retval;
     }
+
+    /**
+     * @deprecated This method is deprecated and will be removed in future versions.
+     */
+    public static function isMobile()
+    {
+        return preg_match("/(android|avantgo|blackberry|bolt|boost|cricket|docomo|fone|hiptop|mini|palm|phone|pie|tablet|up\.browser|up\.link|webos|wos)/i", isset($_SERVER["HTTP_USER_AGENT"]) ? $_SERVER["HTTP_USER_AGENT"] : "");
+    }
+
+    /**
+     * @deprecated This method is deprecated and will be removed in future versions.
+     */
+    public static function isTablet()
+    {
+        return preg_match("/(ipad|tablet)/i", isset($_SERVER["HTTP_USER_AGENT"]) ? $_SERVER["HTTP_USER_AGENT"] : "");
+    }
+
+    /**
+     * @deprecated This method is deprecated and will be removed in future versions.
+     */
+    public static function isBot()
+    {
+        return preg_match("/rambler|abacho|acoi|accona|aspseek|altavista|estyle|scrubby|lycos|geona|ia_archiver|alexa|sogou|skype|facebook|twitter|pinterest|linkedin|naver|bing|google|yahoo|duckduckgo|yandex|baidu|teoma|xing|java\/1.7.0_45|bot|crawl|slurp|spider|mediapartners|\sask\s|\saol\s/i", isset($_SERVER["HTTP_USER_AGENT"]) ? $_SERVER["HTTP_USER_AGENT"] : "");
+    }
     
     public static function getClientIP()
     {
